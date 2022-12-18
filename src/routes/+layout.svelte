7d7970cc434c0fd4430f48b2e8966ec953e28a29
mylesz123 <mylesz123@gmail.com>
2022-12-17 18:09:47 -0700

    basic upload with tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import "../app.css";
+</script>
+
+<div>
+  <nav class="">
+    <div class='flex justify-between px-3 py-2 max-w-7xl m-auto'>
+      <a href='/'>Home</a>
+
+      <div class="flex gap-3">
+        <a href='/flavors'>Flavors</a>
+      </div>
+    </div>
+  </nav>
+  <slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<main>
+  <h1>Sweet Licks</h1>
+  <p>The best ice cream in the world. Ask your mom</p>
+</main>
